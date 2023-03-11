81985accef266245c0e26d8aff38f7dfda3e5b35
NiklasWuerfl <wi21162@lehre.dhbw-stuttgart.de>
2023-03-11 19:38:38 +0100

    Adding basic routes, login and EJS files
    Login functionality implemented (without encryption),
    displaying the username after login
    jwt token generated

--- database.js
+++ database.js
@@ -6,15 +6,28 @@ async function init() {
   await db.serialize(() => {
     db.run('DROP TABLE IF EXISTS users')
     db.run('CREATE TABLE users (userID text PRIMARY KEY UNIQUE, name text, role text, password text)')
-    db.run('INSERT INTO users (userID, role, name, password) VALUES ("id1", "user1", "student", "password")')
-    db.run('INSERT INTO users (userID, role, name, password) VALUES ("id2", "user2", "student", "password2")')
-    db.run('INSERT INTO users (userID, role, name, password) VALUES ("id3", "user3", "teacher", "password3")')
-    db.run('INSERT INTO users (userID, role, name, password) VALUES ("admin", "admin", "admin", "admin")')
+    db.run('INSERT INTO users (userID, name, role, password) VALUES ("id1", "user1", "student", "password")')
+    db.run('INSERT INTO users (userID, name, role, password) VALUES ("id2", "user2", "student", "password2")')
+    db.run('INSERT INTO users (userID, name, role, password) VALUES ("id3", "user3", "teacher", "password3")')
+    db.run('INSERT INTO users (userID, name, role, password) VALUES ("admin", "admin", "admin", "admin")')
+  })
+}
+
+function getUserPassword(password) {
+  return new Promise((resolve, reject) => {
+    let sql = "SELECT * FROM users WHERE password  = ? "
+
+    db.get(sql, [password], (err, row) => {
+      if (row === undefined) reject(`There is no user with the password "${password}" in the database.`)
+      if (!err) resolve(row)
+      else reject(err)
+    })
   })
 }
 
 module.exports = {
   init,
+  getUserPassword
   // getUser,
   // getAllUsernames,
   // addUser
--- server.js
+++ server.js
@@ -7,8 +7,12 @@ const jwt = require('jsonwebtoken')
 require('dotenv').config()
 const port = process.env.PORT
 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+
+let currentKey = ""
+let currentPassword = ""
+let currentUser;
 
 app.listen(port, () => {
   console.log("Server is running on port " + port)
@@ -18,4 +22,52 @@ app.listen(port, () => {
   catch {
     res.status(500).statusMessage('There is a problem with the database or the DB connection')
   }
+})
+
+app.get('/', (req, res) => {
+  res.redirect('/identify')
+})
+
+app.post('/identify', async (req, res) => {
+  const username = req.body.password
+  const token = jwt.sign(username, process.env.ACCESS_TOKEN_SECRET)
+  currentKey = token
+  currentPassword = username
+  try {
+    currentUser = await database.getUserPassword(currentPassword)
+    res.redirect('/granted')
+  }
+  catch {
+    // alert('Invalid Password')
+    console.log('Invalid Password');
+    res.redirect('/identify')
+  }
+})
+
+app.get('/identify', (req, res) => {
+  res.render('identify.ejs')
+})
+
+function authenticateToken(req, res, next) {
+  if (currentKey == "") {
+    res.redirect('/identify')
+  } else if (jwt.verify(currentKey, process.env.ACCESS_TOKEN_SECRET)) {
+    next()
+  } else {
+    res.redirect ('/identify')
+  }
+}
+
+app.get('/granted',authenticateToken, (req, res) => {
+  res.render('start.ejs', {
+    message: `${currentUser.name}`
+  })
+})
+
+function authenticateRole(req, res, next) {
+  
+}
+
+app.get('/admin', authenticateRole ,(req, res) => {
+  res.render('admin.ejs')
 })
