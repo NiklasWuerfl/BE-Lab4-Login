e107a0f5cfd6865357f339045b13f782dacba5e0
NiklasWuerfl <wi21162@lehre.dhbw-stuttgart.de>
2023-03-11 16:17:32 +0100

    Database setup
    Basic setups, DB file is not initialized yet.

---
+++ databse.js
@@ -0,0 +1,16 @@
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('./users.db', sqlite3.OPEN_READWRITE)
+
+
+async function init() {
+  await db.serialize(() => {
+    db.run('DROP TABLE IF EXISTS users')
+    db.run('CREATE TABLE users (userID text PRIMARY KEY UNIQUE, name text, role text, password text)')
+    db.run('INSERT INTO user (userID, role, name, password) VALUES ("id1", "user1", "student", "password")')
+    db.run('INSERT INTO user (userID, role, name, password) VALUES ("id2", "user2", "student", "password2")')
+    db.run('INSERT INTO user (userID, role, name, password) VALUES ("id3", "user3", "teacher", "password3")')
+    db.run('INSERT INTO user (userID, role, name, password) VALUES ("admin", "admin", "admin", "admin")')
+
+
+  })
+}
